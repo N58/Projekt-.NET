7977dbbf002985330a39717ffdf812658e93ff5b
mszczykowski <67839881+mszczykowski@users.noreply.github.com>
2021-05-24 10:41:42 +0200

    Create Favourites.cs

---
+++ Models/Favourites.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalKulinarny.Models
+{
+    public class Favourites
+    {
+        public string UserID { get; set; }
+        public int RecipeID { get; set; }
+    }
+}
