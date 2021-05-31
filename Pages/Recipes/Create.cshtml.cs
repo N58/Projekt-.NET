79b8fc905845034495225154733c936feddbbe79
Krystian Szmigielski <krystian.szmigielski2@gmail.com>
2021-05-31 11:15:46 +0200

    Added Ingredients in Create

--- Pages/Recipes/Create.cshtml.cs
+++ Pages/Recipes/Create.cshtml.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 using PortalKulinarny.Data;
 using PortalKulinarny.Models;
 
@@ -25,12 +27,15 @@ namespace PortalKulinarny.Pages.Recipes
 
         public IActionResult OnGet()
         {
+            RemoveSession(recipeSessionName); // Clearing session when page is new
             return Page();
         }
 
         [BindProperty]
         public Recipe Recipe { get; set; }
-        public List<Ingredient> IngredientsList { get; set; }
+        [BindProperty]
+        public Ingredient NewIngredient { get; set; }
+        private string recipeSessionName = "RecipeCreate";
 
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://aka.ms/RazorPagesCRUD.
@@ -38,22 +43,11 @@ namespace PortalKulinarny.Pages.Recipes
         {
             if (ModelState.IsValid)
             {
+                Recipe = GetSession<Recipe>(recipeSessionName);
                 var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
                 Recipe.UserId = userId;
                 Recipe.DateTime = DateTime.Now;
                 Recipe.ModificationDateTime = DateTime.Now;
-
-                var ingredient = new Ingredient()
-                {
-                    Name = "test"
-                };
-                var ingredient2 = new Ingredient()
-                {
-                    Name = "test2"
-                };
-                Recipe.Ingredients = new List<Ingredient>();
-                Recipe.Ingredients.Add(ingredient);
-                Recipe.Ingredients.Add(ingredient2);
 
                 Recipe.Votes = new List<Vote>();
 
@@ -62,13 +56,46 @@ namespace PortalKulinarny.Pages.Recipes
                 {
                     _context.Recipes.Add(Recipe);
                     await _context.SaveChangesAsync();
+                    RemoveSession(recipeSessionName);
                     return RedirectToPage("./Index");
                 }
 
+                RemoveSession(recipeSessionName);
                 return RedirectToPage("./Index");
             }
 
+            RemoveSession(recipeSessionName);
             return RedirectToPage("./Index");
+        }
+
+        public IActionResult OnPostAddIngredient()
+        {
+            Recipe = GetSession<Recipe>(recipeSessionName);
+            if (Recipe.Ingredients == null)
+                Recipe.Ingredients = new List<Ingredient>();
+            Recipe.Ingredients.Add(NewIngredient);
+            SetSession(recipeSessionName, Recipe);
+
+            return Page(); // reloading page without OnGet()
+        }
+
+        private T GetSession<T>(string name) where T : new()
+        {
+            var jsonSession = HttpContext.Session.GetString(name);
+            if (jsonSession != null)
+                return JsonConvert.DeserializeObject<T>(jsonSession);
+            else
+                return new T();
+        }
+
+        private void SetSession(string name, object obj)
+        {
+            HttpContext.Session.SetString(name, JsonConvert.SerializeObject(obj));
+        }
+
+        private void RemoveSession(string name)
+        {
+            HttpContext.Session.Remove(name);
         }
     }
 }
